1f05d653a5626680f0dd56676b72215bd73cdba2
idsaidov <id.saidov@yandex.ru>
2022-09-01 00:30:15 +0300

    I have selected more suitable types for the subscription field

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         post.patronymic = "Иванович";
         post.phone = "+7 (999)-999-99-99";
         post.surname = "Иванов";
-        post.subscription = "true";
+        post.subscription = true;
 
         post.birthday.day = 13;
         post.birthday.month = 6;
